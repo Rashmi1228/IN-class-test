0135348fce9302eb03a621f6ae223ebc1e9f35f8
Rashmi1228 <46466021+Rashmi1228@users.noreply.github.com>
2019-02-26 21:28:49 -0500

    Add files via upload

---
+++ Create dbo.Product1.sql
@@ -0,0 +1,16 @@
+﻿USE [test1]
+GO
+
+/****** Object: Table [dbo].[Product1] Script Date: 2/26/2019 9:23:21 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Product1] (
+    [product ID]   INT          NOT NULL,
+    [Product name] VARCHAR (50) NULL
+);
+
+
---
+++ Create dbo.product2.sql
@@ -0,0 +1,16 @@
+﻿USE [test1]
+GO
+
+/****** Object: Table [dbo].[product2] Script Date: 2/26/2019 9:23:54 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[product2] (
+    [product code] INT NOT NULL,
+    [product ID]   INT NOT NULL FOREIGN KEY product1(product ID)
+);
+
+
